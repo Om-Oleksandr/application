dfad7a67b93e94404d6bb9ad33fcc8546644b842
Oleksandr Om <razy.uys.razy00@gmail.com>
2023-03-21 14:10:29 +0200

    Do homework

--- src/App.js
+++ src/App.js
@@ -1,48 +1,26 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import "./App.css";
-import Tree from "./components/Tree";
-import { UserContext, ThemeContext, LangContext } from "./contexts/index";
+import { LangContext, ThemeContext, UserContext } from "./contexts";
 import Header from "./components/Header";
 import CONSTANTS from "./constants";
-
-const { THEMES, DEFAULT_LANG } = CONSTANTS;
-
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      user: {
-        id: 1,
-        firstName: "John",
-        lastName: "Doe",
-      },
-      theme: THEMES.DARK,
-      lang: DEFAULT_LANG,
-    };
-  }
-
-  setTheme = (theme) => {
-    this.setState({ theme });
-  };
-
-  setLang = (lang) => {
-    this.setState({ lang });
-  };
-
-  render() {
-    const { user, theme, lang } = this.state;
-    console.log(UserContext);
-    return (
-      <LangContext.Provider value={[lang, this.setLang]}>
-        <ThemeContext.Provider value={[theme, this.setTheme]}>
-          <UserContext.Provider value={user}>
+const { DEFAULT_LANG } = CONSTANTS;
+const App = () => {
+  const [user] = useState({
+    id: 1,
+    firstName: "Naruto",
+    lastName: "Uzumaki",
+  });
+  const [isLight, setIslight] = useState(true);
+  const [lang, setLang] = useState(DEFAULT_LANG);
+  return (
+    <LangContext.Provider value={[lang, setLang]}>
+      <ThemeContext.Provider value={[isLight, setIslight]}>
+        <UserContext.Provider value={user}>
             <Header />
-            <Tree />
-          </UserContext.Provider>
-        </ThemeContext.Provider>
-      </LangContext.Provider>
-    );
-  }
-}
+        </UserContext.Provider>
+      </ThemeContext.Provider>
+    </LangContext.Provider>
+  );
+};
 
 export default App;
